baf30cd4e2b8ebdf192583171026cdecdaa8e3d0
cestevezdev <cestevezdev@gmail.com>
2024-02-10 18:01:39 +0000

    Pull all repos recursively

---
+++ git/pull_all_repos.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Function to update Git repositories
+update_git() {
+    # Save the current path
+    local current_dir="$PWD"
+
+    # Find all directories containing a .git folder
+    for repo in $(find . -type d -name ".git" | sed 's|/.git||'); do
+        echo "Updating repository in: $repo"
+        cd "$repo" # Change to the repository directory
+        git pull   # Update the repository
+        cd "$current_dir" # Return to the initial directory
+    done
+}
+
+# Call the function to start updating
+update_git
+
